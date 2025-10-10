2b695faf04d91d6b9218c1a68833553c025a9314
insiwd <ruan.cavalcante505@gmail.com>
2025-10-10 16:38:38 -0300

    feat: add colorthief to change the colors of the site change the color of the card and background, input + button

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const colorThief = new ColorThief();
+
 async function fetchData() {
   try {
     const username = document.getElementById("username").value.toLowerCase();
-    const response = await fetch(`https://lastcard-fm-backend-v1-0.onrender.com/${username}`);
+    const response = await fetch(
+      `https://lastcard-fm-backend-v1-0.onrender.com/${username}`
+    );
 
     if (!response.ok) {
       throw new Error("Não foi possível localizar");
@@ -13,9 +17,11 @@ async function fetchData() {
     let artistUrl = data.topalbums.album[0].artist.url;
     let albumImage = data.topalbums.album[0].image[3]["#text"];
 
-
+    
     // mudando imagem
     let albumImageSrc = document.getElementById("albumImage");
+    
+    albumImageSrc.crossOrigin = "Anonymous";
     albumImageSrc.src = albumImage;
     albumImageSrc.style.display = "block";
 
@@ -24,24 +30,56 @@ async function fetchData() {
     albumNameParagraph.style.display = "block";
     albumNameParagraph.innerHTML = albumName;
 
-    // mudando nome do artista 
+    // mudando nome do artista
     let artistNameParagraph = document.getElementById("artistNameParagraph");
     artistNameParagraph.style.display = "block";
     artistNameParagraph.innerHTML = artistName;
 
+    // colorThief
+    const colorThiefImg = albumImageSrc;
 
+    function processImage() {
+      const dominantColor = colorThief.getColor(colorThiefImg);
+      console.log("cor dominante: " + dominantColor);
+
+      const colorPalette = colorThief.getPalette(colorThiefImg, 10);
+      console.log("paleta: " + colorPalette);
+
+      const corCarta = colorPalette[1];
+
+      // mudando o bg
+      document.body.style.backgroundColor = `rgb(${colorPalette[0].join(',')})`;
+      
+      // mudando cor da carta
+      let cardColor = document.getElementById("card");
+      cardColor.style.backgroundColor = `rgb(${colorPalette[5].join(',')})`;
+
+      let username = document.getElementById("username");
+      username.style.backgroundColor = `rgb(${colorPalette[5].join(',')})`;
+
+      let button = document.getElementById("button");
+      button.style.backgroundColor = `rgb(${colorPalette[5].join(',')})`;
+    
+    }
+    if (colorThiefImg.complete) {
+      processImage();
+    } else {
+      colorThiefImg.addEventListener('load', processImage);
+    }
+
+    // log no terminal
     console.log(
       "album name: " +
-      albumName +
-      "\n" +
-      "artist name: " +
-      artistName +
-      "\n" +
-      "artist URL: " +
-      artistUrl +
-      "\n" +
-      "Images: " +
-      albumImage
+        albumName +
+        "\n" +
+        "artist name: " +
+        artistName +
+        "\n" +
+        "artist URL: " +
+        artistUrl +
+        "\n" +
+        "Images: " +
+        albumImage
     );
   } catch (error) {
     console.error(error);
